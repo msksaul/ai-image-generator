cfd01a754cd1d0e1f153a7bf821809fe0c111a7b
mscalel <msksaul@gmail.com>
2024-05-29 13:41:32 -0600

    Cloudinary Media Uploader

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -11,6 +11,7 @@ import { Input } from '../ui/input'
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '../ui/select'
 import { AspectRatioKey, debounce, deepMergeObjects } from '@/lib/utils'
 import { Button } from '../ui/button'
+import MediaUploader from './MediaUploader'
 
 export const formSchema = z.object({
   title: z.string(),
@@ -175,6 +176,23 @@ const TransformationForm = ({ action, data = null, userId, type, creditBalance, 
             )}
           </div>
         )}
+
+        <div className='media-uploader-field'>
+          <CustomField
+            control={form.control}
+            name='publicId'
+            className='flex size-full flex-col'
+            render={({ field }) => (
+              <MediaUploader
+                onValueChange={field.onChange}
+                setImage={setImage}
+                publicId={field.value}
+                image={image}
+                type={type}
+              />
+            )}
+          />
+        </div>
 
         <div className='flex flex-col gap-4'>
         <Button
